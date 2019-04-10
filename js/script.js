489293e3f92da08dfa6e01a445e48c6f4fd180a3
chidrome <chidrome@gmail.com>
2019-04-10 16:56:26 -0700

    added logic to show three random products. took me A WHILE but was able to figure it out in the least amount of lines of code

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 
 // Global variables
 var allPics = [];
-var pic1 = document.getElementById('productPic1');
-var pic2 = document.getElementById('productPic2');
-var pic3 = document.getElementById('productPic3');
+var displayedProducts = document.querySelectorAll('.pic');
 
 
 // Create constructor function for the product
 function product(name){
   this.filepath = `assets/${name}.jpg`;
   this.name = name;
+  this.views = 0;
+  this.clicks = 0;
   allPics.push(this);
 }
 
@@ -40,17 +40,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ////////////////////////////// populate the products on the page //////////////////////////////
   showThree();
+  addPicListeners();
 
 });
 
 ////////////////////////////// Helper Functions //////////////////////////////
-function showThree(){
-  pic1.setAttribute('src', `${allPics[0].filepath}`);
-  pic2.setAttribute('src', `${allPics[1].filepath}`);
-  pic3.setAttribute('src', `${allPics[2].filepath}`);
-}
+var showThree = () => {
+  let arrayToShow = [];
+  var numberToPush = generateRandomNumber();
+
+  while(arrayToShow.length !== 3){
+    if(numberToPush < 20 && arrayToShow.indexOf(numberToPush) === -1){
+      arrayToShow.push(numberToPush);
+    } else {
+      numberToPush = generateRandomNumber();
+    }
+
+  for(let i = 0; i < arrayToShow.length; i++){
+    document.getElementById(`productPic${i+1}`).setAttribute('src', `${allPics[arrayToShow[i]].filepath}`);
+    document.getElementById(`productPic${i+1}`).setAttribute('alt', `${allPics[arrayToShow[i]].name}`);
+  }
+};
 
 // add event listeners to the pictures
+var addPicListeners = () => {
+  for(var i = 0; i < displayedProducts.length; i++) {
+    displayedProducts[i].addEventListener('click', handleClicks);
+  }
+};
 
+// handle clicks
+var handleClicks = () => {
+  showThree();
+};
 
-
+// generate a random number between 1 and 20
+var generateRandomNumber = () => {
+  return Math.floor(Math.random() * allPics.length);
+};
