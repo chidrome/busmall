43ff6425bb1c3e367948642afccec9c9fbbb29c2
chidrome <chidrome@gmail.com>
2019-04-11 10:21:14 -0700

    chart functionality good to go. showing both views and clicks

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@
 var allPics = [];
 var displayedProducts = document.querySelectorAll('.pic');
 var totalClicks = 0;
+var clicks = [];
+var views = [];
+var productNames = [];
+var myChart;
 
 
 // Create constructor function for the product
@@ -42,6 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // populate the products on the page
   showThree();
   addPicListeners();
+  drawChart();
 
 });
 
@@ -97,6 +102,11 @@ var handleClicks = (e) => {
 
   // show a new set of three once a product has been clicked
   showThree();
+  updateChartArrays();
+  myChart.update();
+  console.log(productNames);
+  console.log(clicks);
+  console.log(views);
 };
 
 // generate a random number between -1 and 20
@@ -106,40 +116,50 @@ var generateRandomNumber = () => {
 
 //////////////////////////////////////////////////////////// Chart functions ////////////////////////////////////////////////////////////
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
+function updateChartArrays() {
+  for (var i = 0; i < allPics.length; i++) {
+    productNames[i] = allPics[i].name;
+    clicks[i] = allPics[i].clicks;
+    views[i] = allPics[i].views;
+  }
+}
+
+var data = {
+  labels: productNames, // titles array we declared earlier
+  datasets: [{
+    type: 'bar',
+    label: '# of clicks',
+    data: clicks,
+    backgroundColor: '#c21d1d'
+  },{
+    type: 'bar',
+    label: '# of views',
+    data: views,
+    backgroundColor: '#76eb76'
+  }]
+};
+
+function drawChart() {
+  var ctx = document.getElementById('myChart');
+  myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+      responsive: true,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    },
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true
+          max: 10,
+          min: 0,
+          stepSize: 1.0
         }
       }]
     }
-  }
-});
+  });
+}
+
